dc50ccdaef237df584d479bf305aed38d9f02bd1
a-dtya <adithyavinod1943@gmail.com>
2025-07-11 13:10:27 +0530

    #3 implemented pydantic models, parser and prompt-template

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain.chains.qa_with_sources.retrieval import RetrievalQAWithSourcesChain
 from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+
+from pydantic import BaseModel, Field
+from typing import List
 
 def get_text_from_pdf(pdf_path): #pdf_path contains list of pdf files
     text=""
@@ -41,3 +45,34 @@ def store_text_in_vector_store(chunks, embeddings):
     vector_store.save_local("faiss_store")
     return vector_store
 
+
+class MCQ(BaseModel): #example MCQ: What is the capital of France? Options: ["Paris", "London", "Berlin", "Madrid"]
+    question: str = Field(...,description="The MCQ question")
+    options: List[str] = Field(...,min_items=3,max_items=3,description="The options for the MCQ")
+    answer: str = Field(...,description="The answer to the MCQ (it should be one of the options)")
+
+
+class MCQList(BaseModel): #example MCQList: [MCQ(question="What is the capital of France?", options=["Paris", "London", "Berlin"], answer="Paris"), MCQ(question="What is the capital of Germany?", options=["Paris", "London", "Berlin"], answer="Berlin")] 
+    mcqs: List[MCQ] = Field(...,description="The list of MCQs")
+
+def mcqlist_parser(): #to enforce strict JSON formatting for output
+    parser = PydanticOutputParser(return_id="id",pydantic_object=MCQList)
+    return parser
+    
+def get_prompt(): #prompt for MCQ generation
+    parser = mcqlist_parser()
+    prompt = PromptTemplate(
+    template="""
+        You are an MCQ generator for campaign training.
+
+        Generate 5 multiple-choice questions from the following campaign material. Each question must have exactly 3 options and one correct answer.
+
+        {format_instructions}
+
+        Campaign context:
+        {context}
+    """,
+        input_variables=["context"],
+        partial_variables={"format_instructions": parser.get_format_instructions()}
+    )
+    return prompt
